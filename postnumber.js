b317a1e4a1571221bf33892d906cd3052087b794
takuya-wada-0720 <71623100+takuya-wada-0720@users.noreply.github.com>
2020-09-24 13:42:42 +0900

    Update postnumber.js

--- postnumber.js
+++ postnumber.js
@@ -7,7 +7,7 @@ function addValue(){
     var idname = "neko";
     var s = document.getElementById(idname).value;
     if(s.length == 7) {
-        document.write("<script src="https://takuya-wada-0720.github.io/post/program/0-9.js"></script>");
+        document.write("<script src="https://takuya-wada-0720.github.io/post/program/1-9.js"></script>");
         for(var a=0; a < tmp.length; ++a) {
         if(s == data[a][0]) {
           var c = data[a][1]+data[a][2]+data[a][3]+data[a][4];
