74eb2ba275b145ff3782dc3d1ace10319eb05b7c
takuya-wada-0720 <b.takuya.wada@gmail.com>
2020-09-24 14:47:42 +0900

    Update postnumber.js

--- postnumber.js
+++ postnumber.js
@@ -1,13 +1,15 @@
 var postnum;
+var s
 var url = "https://takuya-wada-0720.github.io/post/";
 var tmpdata = [];
 var data = [];
 
 function addValue(){
-    var idname = "neko";
-    var s = document.getElementById(idname).value;
+    postnum = document.getElementById("neko").value;
     if(s.length == 7) {
+        postnum = parseInt(s , 10);
         document.write("<script src=\"https://takuya-wada-0720.github.io/post/program/0-9.js\"></script>");
+        function search1(postnum)
         for(var a=0; a < tmpdata.length; ++a) {
         if(s == data[a][0]) {
           var c = data[a][1]+data[a][2]+data[a][3]+data[a][4];
