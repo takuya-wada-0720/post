d65abf3430c6dff5399378ac1694eea9e7747a26
takuya-wada-0720 <b.takuya.wada@gmail.com>
2020-09-24 13:16:24 +0900

    a

---
+++ program/001-099.js
@@ -0,0 +1,26 @@
+function search2(num2) {
+    switch(num2.slice(0,3)) {
+        case 001:
+            function getCSV(url){
+                var csv = new XMLHttpRequest();
+                csv.open("get",url,true);
+                csv.send(null);
+                csv.onload = function(){
+                    convertCSVtoArray(csv.responseText);
+                }
+            }
+
+            function convertCSVtoArray(str){
+                tmpdata = str.split("\n");
+                for(var i=0;i<tmpdata.length;++i){
+                    data[i] = tmpdata[i].split(',');
+                }
+                console.log(data[0][0]);
+            }
+            return data;
+            break;
+        default:
+            break;
+        }
+    }
+}
