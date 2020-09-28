e97e226657d249addef46d66ba6ec08746b20d18
takuya-wada-0720 <b.takuya.wada@gmail.com>
2020-09-28 13:41:34 +0900

    Update 001-099.js

--- program/001-099.js
+++ program/001-099.js
@@ -40,14 +40,15 @@ function convertCSVtoArray(str){
 function up(d1,d2) {
   if(parseInt(d1[d2.length/2 - 1][0]) < parseInt(postnum)){
     if(parseInt(d1[d2.length/2 - 1 + d2.length/4][0]) < parseInt(postnum)){
-      for(var i = 0;i < d2.length/4;i++){
+      for(var i = 0;i < d2.length/4+1;i++){
         if(d1[d2.length/2 + d2.length/4 + i][0] == postnum){
           document.getElementById("nekopv").value = d1[d2.length/2 + d2.length/4 + i][1]+d1[d2.length/2 + d2.length/4 + i][2]+d1[d2.length/2 + d2.length/4 + i][3]+d1[d2.length/2 + d2.length/4 + i][4];
           break;
         }
       }
+    }
     else{
-      for(var i = 0;i < d2.length/4;i++){
+      for(var i = 0;i < d2.length/4+1;i++){
         if(d1[d2.length/2 + i][0] == postnum){
           document.getElementById("nekopv").value = d1[d2.length/2 + i][1]+d1[d2.length/2 + i][2]+d1[d2.length/2 + i][3]+d1[d2.length/2 + i][4];
           break;
@@ -57,20 +58,20 @@ function up(d1,d2) {
   }
   else {
     if(parseInt(d1[d2.length/4 - 1][0]) < parseInt(postnum)){
-      for(var i = 0;i < d2.length/4;i++){
+      for(var i = 0;i < d2.length/4+1;i++){
         if(d1[d2.length/4 + i][0] == postnum){
           document.getElementById("nekopv").value = d1[d2.length/4 + i][1]+d1[d2.length/4 + i][2]+d1[d2.length/4 + i][3]+d1[d2.length/4 + i][4];
           break;
         }
       }
+    }
     else{
-      for(var i = 0;i < d2.length/4;i++){
+      for(var i = 0;i < d2.length/4+1;i++){
         if(d1[i][0] == postnum){
           document.getElementById("nekopv").value = d1[i][1]+d1[i][2]+d1[i][3]+d1[i][3];
           break;
         }
       }
     }
-
   }
 }
